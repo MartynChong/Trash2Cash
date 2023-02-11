4f9aa16b8f467c58fcfa208e660b4bfca1410556
martyncqt <martyncqt@gmail.com>
2023-02-11 20:31:33 +0000

    Added QR scan

---
+++ app/src/main/java/com/example/trash2cash/Capture.java
@@ -0,0 +1,7 @@
+package com.example.trash2cash;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity{
+
+}
--- app/src/main/java/com/example/trash2cash/MainActivity.java
+++ app/src/main/java/com/example/trash2cash/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.trash2cash;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -18,11 +20,14 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.trash2cash.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     FirebaseUser user;
 
+    Button btScan;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,5 +64,59 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        btScan = findViewById(R.id.bt_scan);
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(
+                        MainActivity.this
+                );
+                intentIntegrator.setPrompt("For flash use volume up key");
+                //Set beep
+                intentIntegrator.setBeepEnabled(true);
+                //Locked orientation
+                intentIntegrator.setOrientationLocked(true);
+                //Set capture activity
+                intentIntegrator.setCaptureActivity(Capture.class);
+                //Initiate scan
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Initialise intent results
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(
+                requestCode, requestCode, data
+        );
+        //Check condition
+        if (intentResult.getContents() != null){
+            //When result content is not null
+            //Initialise alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    MainActivity.this
+            );
+            //Set title
+            builder.setTitle("Result");
+            //Set message
+            builder.setMessage(intentResult.getContents());
+            //Set positive button
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //Dismiss dialog
+                    dialogInterface.dismiss();
+                }
+            });
+            //Show alert dialog
+            builder.show();
+        }else {
+            //When result content is null, display toast
+            Toast.makeText(getApplicationContext(), "Ooops, no scan", Toast.LENGTH_SHORT)
+            .show();
+        }
     }
 }
